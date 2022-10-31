16bfb56b60a3957fb1b1a2851691a3db1b23a42a
NgoHuuNghia <silassunsolaire@gmail.com>
2022-10-31 18:08:19 +0700

    before roles init

---
+++ src/features/notes/NotesList.jsx
@@ -0,0 +1,54 @@
+import { useGetNotesQuery } from "./notesApiSlice";
+import Note from "./Note";
+
+const NotesList = () => {
+  const {
+    data: notes,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetNotesQuery("notesList", {
+    pollingInterval: 15000,
+    refetchOnFocus: true,
+    refetchOnMountOrArgChange: true,
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p className="errmsg">{error?.data?.message}</p>;
+  if (isSuccess) {
+    const { ids } = notes;
+    return (
+      <table className="table table--notes">
+        <thead className="table__thead">
+          <tr>
+            <th scope="col" className="table__th note__status">
+              Username
+            </th>
+            <th scope="col" className="table__th note__created">
+              Created
+            </th>
+            <th scope="col" className="table__th note__updated">
+              Updated
+            </th>
+            <th scope="col" className="table__th note__title">
+              Title
+            </th>
+            <th scope="col" className="table__th note__username">
+              Owner
+            </th>
+            <th scope="col" className="table__th note__edit">
+              Edit
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {ids?.length
+            ? ids.map((noteId) => <Note key={noteId} noteId={noteId} />)
+            : null}
+        </tbody>
+      </table>
+    );
+  }
+};
+export default NotesList;
---
+++ src/features/users/UsersList.jsx
@@ -0,0 +1,47 @@
+import User from "./User";
+import { useGetUsersQuery } from "./usersApiSlice";
+
+const UsersList = () => {
+  const {
+    data: users,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetUsersQuery("usersList", {
+    pollingInterval: 60000,
+    refetchOnFocus: true,
+    refetchOnMountOrArgChange: true,
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p className="errmsg">{error?.data?.message}</p>;
+  if (isSuccess) {
+    const { ids } = users;
+
+    return (
+      <table className="table table--users">
+        <thead className="table__thead">
+          <tr>
+            <th scope="col" className="table__th user__username">
+              Username
+            </th>
+            <th scope="col" className="table__th user__roles">
+              Roles
+            </th>
+            <th scope="col" className="table__th user__edit">
+              Edit
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {ids?.length
+            ? ids.map((userId) => <User key={userId} userId={userId} />)
+            : null}
+        </tbody>
+      </table>
+    );
+  }
+};
+
+export default UsersList;
